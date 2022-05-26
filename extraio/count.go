e9cea648c10dabe165a9e75a2c96872561885b10
Ivan Trubach <mr.trubach@icloud.com>
2022-05-26 18:59:38 +0300

    refactor(extraio): handle overflow in CountReader

--- extraio/count.go
+++ extraio/count.go
@@ -2,29 +2,38 @@ package extraio
 
 import (
 	"io"
+	"math"
 )
 
 // CountReader counts bytes read from the underlying io.Reader.
 type CountReader struct {
-	r io.Reader // underlying reader
-	n uint64
+	reader   io.Reader
+	count    uint64 // mutable
+	overflow bool   // mutable
 }
 
 // NewCountReader returns a new reader that counts bytes read from r.
 func NewCountReader(r io.Reader) *CountReader {
-	return &CountReader{r: r}
+	return &CountReader{reader: r}
 }
 
-// Count returns the count of bytes read.
-func (r *CountReader) Count() uint64 {
-	return r.n
+// Count returns the count of bytes read. It returns false if the count of read
+// bytes cannot be represented as 64 bit unsigned integer. In practice, that
+// would require counting thousands of petabytes to reach this limitation.
+func (r *CountReader) Count() (uint64, bool) {
+	return r.count, !r.overflow
 }
 
 // Read implements io.Reader interface. It reads from the underlying io.Reader.
 func (r *CountReader) Read(p []byte) (int, error) {
-	n, err := r.r.Read(p)
-	if n > 0 {
-		r.n += uint64(n)
+	n, err := r.reader.Read(p)
+	if n > 0 && !r.overflow {
+		nn := uint64(n)
+		r.count += nn
+		if r.count < nn {
+			r.overflow = true
+			r.count = math.MaxUint64
+		}
 	}
 	return n, err
 }
